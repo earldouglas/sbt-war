4a6b53f4fdfaaa347672816e01f7fbab27b3a5b7
Artyom Olshevskiy <siasiamail@gmail.com>
2011-04-25 02:00:00 +0300

    Eliminate copy paste and add configurability to tasks

--- src/main/scala/WebPlugin.scala
+++ src/main/scala/WebPlugin.scala
@@ -9,7 +9,16 @@ object WebPlugin extends Plugin {
 
   val temporaryWarPath = SettingKey[Path]("temporary-war-path")
   val webappResources = SettingKey[PathFinder]("webapp-resources")
+  val webappUnmanaged = SettingKey[Seq[File]]("webapp-unmanaged")
   val prepareWebapp = TaskKey[Unit]("prepare-webapp")
+  val jettyClasspaths = TaskKey[JettyClasspaths]("jetty-classpaths")
+  final case class JettyClasspaths(classpath: PathFinder, jettyClasspath: PathFinder)
+  val jettyContext = SettingKey[String]("jetty-context")
+  val jettyScanDirs = SettingKey[Seq[Path]]("jetty-scan-dirs")
+  val jettyScanInterval = SettingKey[Int]("jetty-scan-interval")
+  val jettyPort = SettingKey[Int]("jetty-port")
+  val jettyConfFiles = SettingKey[JettyConfFiles]("jetty-conf-files")
+  final case class JettyConfFiles(env: Option[File], webDefaultXml: Option[File])
   val jettyConfiguration = TaskKey[JettyConfiguration]("jetty-configuration")
   private var _jettyInstance: Option[JettyRunner] = None
   val jettyInstance = TaskKey[JettyRunner]("jetty-instance")
@@ -17,11 +26,9 @@ object WebPlugin extends Plugin {
   val jettyStop = TaskKey[Unit]("jetty-stop")
   val jettyReload = TaskKey[Unit]("jetty-reload")
 
-  def prepareWebappTask(webappContents: PathFinder, warPath: Path, classpath: PathFinder, defaultExcludes: FileFilter, slog: Logger) {
+  def prepareWebappTask(webappContents: PathFinder, warPath: Path, classpath: PathFinder, extraJars: PathFinder, ignore: PathFinder, defaultExcludes: FileFilter, slog: Logger) {
     val log = slog.asInstanceOf[AbstractLogger]
     def descendents(parent: PathFinder, include: FileFilter) = parent.descendentsExcept(include, defaultExcludes)
-    val extraJars = Path.emptyPathFinder
-    val ignore = Path.emptyPathFinder
     import sbt.classpath.ClasspathUtilities
     val webInfPath = warPath / "WEB-INF"
     val webLibDirectory = webInfPath / "lib"
@@ -32,50 +39,52 @@ object WebPlugin extends Plugin {
     if(log.atLevel(Level.Debug))
       directories.foreach(d => log.debug(" Copying the contents of directory " + d + " to " + classesTargetDirectory))
 
-    import sbt.oldcompat.{copy, copyFlat, copyFilesFlat, clean}
+    import sbt.oldcompat.{copy, copyFlat, clean}
     (copy(webappContents.get, warPath, log).right flatMap { copiedWebapp =>
       copy(classesAndResources.get, classesTargetDirectory, log).right flatMap { copiedClasses =>
-	copyFlat(libs, webLibDirectory, log).right flatMap { copiedLibs =>
-	  copyFilesFlat(extraJars.getFiles, webLibDirectory, log).right flatMap { copiedExtraLibs => {
+	copyFlat(libs ++ extraJars.get, webLibDirectory, log).right flatMap { copiedLibs =>
 	    val toRemove = scala.collection.mutable.HashSet(((warPath ** "*") --- ignore).get.toSeq : _*)
 	    toRemove --= copiedWebapp
 	    toRemove --= copiedClasses
 	    toRemove --= copiedLibs
-	    toRemove --= copiedExtraLibs
 	    val (directories, files) = toRemove.toList.partition(_.isDirectory)
             if(log.atLevel(Level.Debug))
 	      files.foreach(r => log.debug("Pruning file " + r))
 	    val result =
-	      clean(files, true, log) orElse {
+	      clean(files, log) orElse {
 		val emptyDirectories = directories.filter(directory => directory.asFile.listFiles.isEmpty)
                 if(log.atLevel(Level.Debug))
 		  emptyDirectories.foreach(r => log.debug("Pruning directory " + r))
-		clean(emptyDirectories, true, log)
+		clean(emptyDirectories, log)
 	      }
 	    result.toLeft(())
 	  }
-	}}}}).left.toOption
+	}}).left.toOption foreach error
   }
 
-  def jettyConfigurationTask: Initialize[Task[JettyConfiguration]] = (fullClasspath in Runtime, managedClasspath in jettyConf, temporaryWarPath, scalaInstance, streams) map { (cp, jettyCp, warPath, scalaInstance, s) =>
+  def jettyClasspathsTask(cp: Classpath, jettyCp: Classpath) =
+    JettyClasspaths(cp.map(_.data), jettyCp.map(_.data))
+
+  def jettyConfigurationTask: Initialize[Task[JettyConfiguration]] = (jettyClasspaths, temporaryWarPath, jettyContext, scalaInstance, jettyScanDirs, jettyScanInterval, jettyPort, jettyConfFiles, streams) map { (classpaths, warPath, context, scalaInstance, scanDirs, interval, jettyPort, confs, s) =>
     new DefaultJettyConfiguration {
-      def classpath = Path.finder(cp.map(_.data))
-      def jettyClasspath = Path.finder(jettyCp.map(_.data))
+      def classpath = classpaths.classpath
+      def jettyClasspath = classpaths.jettyClasspath
       def war = warPath
-      def contextPath = "/"
+      def contextPath = context
       def classpathName = jettyConf.toString
       def parentLoader = scalaInstance.loader
-      def scanDirectories = Seq(warPath)
-      def scanInterval = JettyRunner.DefaultScanInterval
-      def port = JettyRunner.DefaultPort
+      def scanDirectories = scanDirs
+      def scanInterval = interval
+      def port = jettyPort
       def log = s.log.asInstanceOf[AbstractLogger]
-      def jettyEnv = None
-      def webDefaultXml = None
+      def jettyEnv = confs.env
+      def webDefaultXml = confs.webDefaultXml
     }
   }
     
 
-  def jettyInstanceTask: Initialize[Task[JettyRunner]] = (jettyConfiguration) map { (conf) =>    
+  def jettyInstanceTask: Initialize[Task[JettyRunner]] = (jettyConfiguration) map {
+    (conf) =>
     if(_jettyInstance.isEmpty) 
       _jettyInstance = Some(new JettyRunner(conf))
     _jettyInstance.get
@@ -101,10 +110,17 @@ object WebPlugin extends Plugin {
       (sd, defaultExcludes) =>
         ((sd / "webapp") ###).descendentsExcept("*", defaultExcludes)
     },
-    prepareWebapp <<= (compile in Runtime, copyResources in Runtime, webappResources, temporaryWarPath, fullClasspath in Runtime, defaultExcludes, streams) map {
-      (c, r, w, wp, cp, excludes, s) =>
-        prepareWebappTask(w, wp, Path.finder(cp.map(_.data)), excludes, s.log) },
+    webappUnmanaged := Seq(),
+    prepareWebapp <<= (compile in Runtime, copyResources in Runtime, webappResources, temporaryWarPath, jettyClasspaths, scalaInstance, webappUnmanaged, defaultExcludes, streams) map {
+      (c, r, w, wp, cp, si, wu, excludes, s) =>
+        prepareWebappTask(w, wp, cp.classpath, Seq(si.libraryJar, si.compilerJar), wu, excludes, s.log) },
     managedClasspath in jettyConf <<= (classpathTypes, update) map { (ct, report) => Classpaths.managedJars(jettyConf, ct, report)},
+    jettyClasspaths <<= (fullClasspath in Runtime, managedClasspath in jettyConf) map jettyClasspathsTask,
+    jettyContext := "/",
+    jettyScanDirs <<= (temporaryWarPath) { (warPath) => Seq(warPath) },
+    jettyScanInterval := JettyRunner.DefaultScanInterval,
+    jettyPort := JettyRunner.DefaultPort,
+    jettyConfFiles := JettyConfFiles(None, None),
     jettyConfiguration <<= jettyConfigurationTask,
     jettyInstance <<= jettyInstanceTask,
     jettyRun <<= jettyRunTask,
--- src/main/scala/sbt/oldcompat.scala
+++ src/main/scala/sbt/oldcompat.scala
@@ -1,190 +1,27 @@
 package sbt
 
 import sbt._
-import IO.{copyFile, wrapNull, createDirectory}
+import IO.{copy, delete, wrapNull, createDirectory}
 
 object oldcompat {
-  def trapUnit(errorMessagePrefix: => String, log: Logger)(execute: => Option[String]): Option[String] =
-    try { execute }
-    catch { case e => log.trace(e); Some(errorMessagePrefix + e.toString) }
+  private def trap[T](log: Logger)(execute: => T): Either[String, T] =
+    try { Right(execute) }
+    catch { case e => log.trace(e); Left(e.toString) }
 
-  def trap[T](errorMessagePrefix: => String, log: Logger)(execute: => Either[String, T]): Either[String, T] =
-    try { execute }
-    catch { case e => log.trace(e); Left(errorMessagePrefix + e.toString) }
+  private def trapUnit(log: Logger)(execute: => Unit): Option[String] =
+    try { execute; None }
+    catch { case e => log.trace(e); Some(e.toString) }
 
-  private def copyImpl(sources: Iterable[Path], destinationDirectory: Path, log: Logger)
-  (doCopy: Path => Option[String]): Option[String] = {
-    val target = destinationDirectory.asFile
-    val creationError = 
-      if(target.isDirectory)
-	None
-      else { 
-	createDirectory(target)
-        None
-      }
-    def copy(sources: List[Path]): Option[String] = {
-      sources match
-      {
-	case src :: remaining => {
-	  doCopy(src) match {
-	    case None => copy(remaining)
-	    case error => error
-	  }
-	}
-	case Nil => None
-      }
-    }
-    creationError orElse ( trapUnit("", log) { copy(sources.toList) } )
+  def copy(sources: Iterable[Path], destinationDirectory: Path, log: Logger): Either[String, Set[Path]] = {
+    val map = sources.map(source => (source.asFile, Path.fromString(destinationDirectory, source.relativePath).asFile))
+    trap(log)(IO.copy(map))
+  }
+
+  def copyFlat(sources: Iterable[Path], destinationDirectory: Path, log: Logger): Either[String, Set[Path]] = {
+    val map = sources.map(source => (source.asFile, Path.fromString(destinationDirectory, source.getName).asFile))
+    trap(log)(IO.copy(map))
   }
   
-  def copy(sources: Iterable[Path], destinationDirectory: Path, log: Logger): Either[String, Set[Path]] =
-    copy(sources, destinationDirectory, false, log)
-
-  /**
-   * Equivalent to {@code copy(sources, destinationDirectory, overwrite, false, log)}.
-   */
-  def copy(sources: Iterable[Path], destinationDirectory: Path, overwrite: Boolean, log: Logger): Either[String, Set[Path]] =
-    copy(sources, destinationDirectory, overwrite, false, log)
-
-  /** Copies the files declared in <code>sources</code> to the <code>destinationDirectory</code>
-   * directory.  Directories are not recursively entered.  The destination hierarchy matches the
-   * source paths relative to any base directories.  For example:
-   *
-   * A source <code>(basePath ##) / x / y</code> is copied to <code>destinationDirectory / x / y</code>.
-   *
-   * @param overwrite if true, existing destination files are always overwritten
-   * @param preserveLastModified if true, the last modified time of copied files will be set equal to
-   * their corresponding source files.
-   */
-  def copy(sources: Iterable[Path], destinationDirectory: Path, overwrite: Boolean, preserveLastModified: Boolean, log: Logger): Either[String, Set[Path]] = {
-    val targetSet = new scala.collection.mutable.HashSet[Path]
-    copyImpl(sources, destinationDirectory, log)
-    {
-      source =>
-	{
-	  val from = source.asFile
-	  val toPath = Path.fromString(destinationDirectory, source.relativePath)
-	  targetSet += toPath
-	  val to = toPath.asFile
-	  if(!to.exists || overwrite || from.lastModified > to.lastModified)
-	    {
-	      val result: Option[String] =
-		if(from.isDirectory) { 
-		  createDirectory(to)
-                  None
-                }
-		else
-		  {
-		    log.debug("Copying " + source + " to " + toPath)
-		    copyFile(from, to)
-                    None
-		  }
-	      if (result.isEmpty && preserveLastModified) { 
-		to.setLastModified(from.lastModified)
-                None
-              }
-	      else
-		result
-	    }
-	      else
-		None
-	}
-    }.toLeft(Set() ++ targetSet)
-  }
-
-  /** Copies the files declared in <code>sources</code> to the <code>targetDirectory</code>
-   * directory.  The source directory hierarchy is flattened so that all copies are immediate
-   * children of <code>targetDirectory</code>.  Directories are not recursively entered.*/
-  def copyFilesFlat(sources: Iterable[File], targetDirectory: Path, log: Logger) = {
-    require(targetDirectory.asFile.isDirectory, "Target '" + targetDirectory + "' is not a directory.")
-    val byName = new scala.collection.mutable.HashMap[String, File]
-    for(source <- sources) byName.put(source.getName, source)
-    val uniquelyNamedSources = byName.values
-    val targetSet = new scala.collection.mutable.HashSet[Path]
-    def copy(source: File): Option[String] =
-      {
-	if(source.isDirectory)
-	  copyAll(source.listFiles.toList)
-	else if(source.exists)
-	  {
-	    val targetPath = targetDirectory / source.getName
-	    targetSet += targetPath
-	    if(!targetPath.exists || source.lastModified > targetPath.lastModified)
-	      {
-		log.debug("Copying " + source + " to " + targetPath)
-		copyFile(source, targetPath.asFile)
-                None
-	      }
-	    else
-	      None
-	  }
-	    else
-	      None
-      }
-    def copyAll(sources: List[File]): Option[String] =
-      sources match
-    {
-      case head :: tail =>
-	copy(head) match
-      {
-	case None => copyAll(tail)
-	case x => x
-      }
-      case Nil => None
-    }
-    trap("Error copying files: ", log) { copyAll(uniquelyNamedSources.toList).toLeft(Set() ++ targetSet) }
-  }
-  /** Copies the files declared in <code>sources</code> to the <code>destinationDirectory</code>
-   * directory.  The source directory hierarchy is flattened so that all copies are immediate
-   * children of <code>destinationDirectory</code>.  Directories are not recursively entered.*/
-  def copyFlat(sources: Iterable[Path], destinationDirectory: Path, log: Logger) = {
-    val targetSet = new scala.collection.mutable.HashSet[Path]
-    copyImpl(sources, destinationDirectory, log) {
-      source => {
-	val from = source.asFile
-	val toPath = destinationDirectory / from.getName
-	targetSet += toPath
-	val to = toPath.asFile
-	if(!to.exists || from.lastModified > to.lastModified && !from.isDirectory)
-	  {
-	    log.debug("Copying " + source + " to " + toPath)
-	    copyFile(from, to)
-            None
-	  }
-	else
-	  None
-      }
-    }.toLeft(Set() ++ targetSet)
-  }
-  /** Deletes the given file recursively.*/
-  def clean(file: Path, log: Logger): Option[String] = clean(file :: Nil, log)
-  /** Deletes the given files recursively.*/
-  def clean(files: Iterable[Path], log: Logger): Option[String] = clean(files, false, log)
-  /** Deletes the given files recursively.  <code>quiet</code> determines the logging level.
-   * If it is true, each file in <code>files</code> is logged at the <code>info</code> level.
-   * If it is false, the <code>debug</code> level is used.*/
-  def clean(files: Iterable[Path], quiet: Boolean, log: Logger): Option[String] =
-    deleteFiles(Path.getFiles(files), quiet, log)
-  
-  private def deleteFiles(files: Iterable[File], quiet: Boolean, log: Logger): Option[String] =
-    ((None: Option[String]) /: files)( (result, file) => result orElse delete(file, quiet, log))
-  private def delete(file: File, quiet: Boolean, log: Logger): Option[String] =
-    {
-      def logMessage(message: => String)
-      {
-	log.log(if(quiet) Level.Debug else Level.Info, message)
-      }
-      trapUnit("Error deleting file " + file + ": ", log) {
-	if(file.isDirectory) {
-	  logMessage("Deleting directory " + file)
-	  deleteFiles(wrapNull(file.listFiles), true, log)
-	  file.delete
-	}
-	else if(file.exists) {
-	  logMessage("Deleting file " + file)
-	  file.delete
-	}
-        None
-      }
-    }
+  def clean(files: Iterable[Path], log: Logger): Option[String] =
+    trapUnit(log)(delete(Path.getFiles(files)))
 }
