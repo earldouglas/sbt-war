d5593ee71cb1ab3da83c6c86c40799244808850b
Artyom Olshevskiy <siasiamail@gmail.com>
2011-05-04 00:52:33 +0300

    Jetty 7.2 support testing

--- src/sbt-test/web/servlet/project/Build.scala
+++ src/sbt-test/web/servlet/project/Build.scala
@@ -17,14 +17,19 @@ object MyBuild extends Build {
 	)
 
 	def libDeps =
-		if(new File("jetty7.3") exists)
-			jetty73Dependencies
+		if(new File("jetty7.2") exists)
+			jetty72Dependencies
+		else if(new File("jetty7.3") exists)
+			jetty73Dependencies	
 		else
 			jetty6Dependencies
 
 	def jetty6Dependencies =
 		Seq("javax.servlet" % "servlet-api" % "2.5" % "provided",
-		"org.mortbay.jetty" % "jetty" % "6.1.22" % "jetty")
+				"org.mortbay.jetty" % "jetty" % "6.1.22" % "jetty")
+	def jetty72Dependencies =
+		Seq("javax.servlet" % "servlet-api" % "2.5" % "provided",
+		"org.eclipse.jetty" % "jetty-webapp" % "7.2.2.v20101205" % "jetty")
 	def jetty73Dependencies =
 		Seq("javax.servlet" % "servlet-api" % "2.5" % "provided",
 		"org.eclipse.jetty" % "jetty-webapp" % "7.3.0.v20110203" % "jetty")
