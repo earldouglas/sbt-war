03ee5b15da4498761bd368fba624ab2caa61a943
Artyom Olshevskiy <siasiamail@gmail.com>
2011-10-06 03:07:34 +0300

    Adjusted deploy script to get passphrase on start

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,18 @@ if [[ ! $1 ]]; then
 		echo "Please provide artifact version"
 else
 VERSION=$1
-TARGET=target/scala-2.9.1/sbt-0.11.0/xsbt-web-plugin-
+TARGET=target/scala-2.9.1/sbt-0.11.0/xsbt-web-plugin_2.9.1-0.11.0-
 POM=${TARGET}${VERSION}.pom
+
+stty -echo 
+read -p "GPG passphrase: " passw; echo 
+stty echo
 
 function mvn-gpg {
 		if [[ $2 ]]; then
 				CLASSIFIER=-Dclassifier=$2
 		fi
-		mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=${POM} -Dfile=${TARGET}${VERSION}$1 $CLASSIFIER
+		mvn gpg:sign-and-deploy-file -Dgpg.passphrase=$passw -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=${POM} -Dfile=${TARGET}${VERSION}$1 $CLASSIFIER
 }
 
 mvn-gpg .jar
