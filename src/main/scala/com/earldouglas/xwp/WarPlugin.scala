293580a15505bf7ff26f79fc44244a7585d5b711
Maxim Fedorov <m.fedorov@thenewmotion.com>
2016-01-14 21:23:05 +0100

    Set .war file as main artifact for WarPlugin

--- src/main/scala/com/earldouglas/xwp/WarPlugin.scala
+++ src/main/scala/com/earldouglas/xwp/WarPlugin.scala
@@ -3,13 +3,9 @@ package com.earldouglas.xwp
 import sbt._, Keys._
 
 object WarPlugin extends AutoPlugin {
-  import Keys.{`package` => pkg}
-
   override def requires = WebappPlugin
 
   override lazy val projectSettings =
-    Defaults.packageTaskSettings(pkg, WebappPlugin.autoImport.webappPrepare) ++
-      Seq(artifact in pkg := Artifact(moduleName.value, "war", "war")) ++
-      addArtifact(artifact in (Compile, pkg), pkg)
-
+    Defaults.packageTaskSettings(packageBin, WebappPlugin.autoImport.webappPrepare) ++
+    Seq(artifact := Artifact(moduleName.value, "war", "war"))
 }
