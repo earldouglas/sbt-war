584030fe12472d75f614a801263113c566f8b2d4
James Earl Douglas <james@earldouglas.com>
2020-03-23 09:01:32 -0600

    Add a simple async servlet example

---
+++ docs/examples/async/src/main/scala/AsyncServlet.scala
@@ -0,0 +1,39 @@
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class AsyncServlet extends HttpServlet {
+
+  val execSvc = java.util.concurrent.Executors.newFixedThreadPool(8)
+
+  override def destroy(): Unit = {
+    execSvc.shutdown
+  }
+
+  override def service( req: HttpServletRequest
+                      , res: HttpServletResponse
+                      ) {
+
+    val ctx = req.startAsync
+
+    execSvc submit {
+      new Runnable() {
+        override def run(): Unit = {
+
+          res.setContentType("text/html")
+          res.setCharacterEncoding("UTF-8")
+
+          val responseBody: String =
+            """<html>
+              |  <body>
+              |    <h1>Hello, world!</h1>
+              |  </body>
+              |</html>""".stripMargin
+          res.getWriter.write(responseBody)
+
+          ctx.complete
+        }
+      }
+    }
+  }
+}
