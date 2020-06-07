26bd00f620c92c51851f99db7ce1fdb60bbe9ae2
James Earl Douglas <james@earldouglas.com>
2020-06-07 12:55:25 -0600

    Set up SSL in beforeAll

--- src/sbt-test/examples/https/src/test/scala/TestSuite.scala
+++ src/sbt-test/examples/https/src/test/scala/TestSuite.scala
@@ -3,27 +3,29 @@ import javax.net.ssl._
 import org.scalatest._
 import java.security.SecureRandom
 
-class TestSuite extends FunSuite with Matchers {
+class TestSuite extends FunSuite with BeforeAndAfterAll with Matchers {
 
-  val sslContext = SSLContext.getInstance("SSL")
-  sslContext.init(
-    null,
-    Array(
-      new X509TrustManager {
-        val getAcceptedIssuers = null
-        def checkClientTrusted(x509Certificates: Array[X509Certificate], s: String) = {}
-        def checkServerTrusted(x509Certificates: Array[X509Certificate], s: String) = {}
+  override def beforeAll(): Unit = {
+    val sslContext = SSLContext.getInstance("SSL")
+    sslContext.init(
+      null,
+      Array(
+        new X509TrustManager {
+          val getAcceptedIssuers = null
+          def checkClientTrusted(x509Certificates: Array[X509Certificate], s: String) = {}
+          def checkServerTrusted(x509Certificates: Array[X509Certificate], s: String) = {}
+        }
+      ),
+      new SecureRandom()
+    )
+
+    HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory)
+    HttpsURLConnection.setDefaultHostnameVerifier(
+      new HostnameVerifier {
+        def verify(s: String, sslSession: SSLSession) = true
       }
-    ),
-    new SecureRandom()
-  )
-
-  HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory)
-  HttpsURLConnection.setDefaultHostnameVerifier(
-    new HostnameVerifier {
-      def verify(s: String, sslSession: SSLSession) = true
-    }
-  )
+    )
+  }
 
   test("/") {
     Request("GET", "https://localhost:8443/", Map.empty, None) shouldBe
