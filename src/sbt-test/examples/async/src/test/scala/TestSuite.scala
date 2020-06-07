d6e583a0eac1d1686ec7b93f533327982dab8840
James Earl Douglas <james@earldouglas.com>
2020-06-07 12:02:52 -0600

    Switch to Http

---
+++ src/sbt-test/examples/async/src/test/scala/TestSuite.scala
@@ -0,0 +1,15 @@
+import org.scalatest._
+
+class TestSuite extends FunSuite with Matchers {
+
+  test("add a message") {
+    Request("GET", "http://localhost:8080/", Map.empty, None) shouldBe
+      Response(200, Map("Content-Type" -> "text/html;charset=utf-8"),
+        """|<html>
+           |  <body>
+           |    <h1>Hello, world!</h1>
+           |  </body>
+           |</html>""".stripMargin
+      )
+  }
+}
