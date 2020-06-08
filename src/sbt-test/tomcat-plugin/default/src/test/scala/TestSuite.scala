3ac0d42c322550da479c0d45bba7fb6a32c79332
James Earl Douglas <james@earldouglas.com>
2020-06-08 17:45:04 -0600

    Update to webapp-runner 9.0.31.0

---
+++ src/sbt-test/tomcat-plugin/default/src/test/scala/TestSuite.scala
@@ -0,0 +1,21 @@
+import org.scalatest._
+
+class TestSuite extends FunSuite with Matchers {
+
+  test("/") {
+    Request("GET", "http://localhost:8080/", Map.empty, None) shouldBe     
+      Response(
+        200,
+        Map(
+          "Connection" -> "keep-alive",
+          "Content-Type" -> "text/html;charset=UTF-8",
+          "Keep-Alive" -> "timeout=60"
+        ),
+        """|<html>
+           |  <body>
+           |    <h1>Hello, world!</h1>
+           |  </body>
+           |</html>""".stripMargin
+      )
+  }
+}
