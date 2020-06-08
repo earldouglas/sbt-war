b4cf19f196b486deb9b78eee932fe31a68641e86
James Earl Douglas <james@earldouglas.com>
2020-06-08 17:12:49 -0600

    Move spray example from docs to scripted

---
+++ src/sbt-test/examples/spray/src/test/scala/TestSuite.scala
@@ -0,0 +1,9 @@
+import org.scalatest._
+
+class TestSuite extends FunSuite with Matchers {
+
+  test("/") {
+    Request("GET", "http://localhost:8080/", Map.empty, None) shouldBe
+      Response(status = 200, Map("Content-Type" -> "text/plain;charset=utf-8"), "Hello, world!")
+  }
+}
