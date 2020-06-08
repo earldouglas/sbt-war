21aedc90596ac37b627d8e3c3c85831e009a3ca5
James Earl Douglas <james@earldouglas.com>
2020-06-08 17:07:53 -0600

    Move scalatra example from docs to scripted

--- src/sbt-test/examples/scalatra/src/test/scala/HelloWorldServletSuite.scala
+++ src/sbt-test/examples/scalatra/src/test/scala/HelloWorldServletSuite.scala
@@ -1,23 +1,22 @@
 import org.scalatest.FunSuiteLike
+import org.scalatest.Matchers
 import org.scalatra.test.scalatest.ScalatraSuite
 
-class HelloWorldServletSuite extends ScalatraSuite with FunSuiteLike {
+class HelloWorldServletSuite extends ScalatraSuite with FunSuiteLike with Matchers {
 
   addServlet(classOf[HelloWorldServlet], "/*")
 
   test("get /") {
     get(uri = "/") {
-      status should equal (200)
-      response.headers.get("Content-Type") should equal {
+      status shouldBe 200
+      response.headers.get("Content-Type") shouldBe
         Some(List("text/html; charset=UTF-8"))
-      }
-      body should equal {
+      body shouldBe
         """<html>
           |  <body>
           |    <h1>Hello, world!</h1>
           |  </body>
           |</html>""".stripMargin
-      }
     }
   }
 }
