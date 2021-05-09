6c7867a92f89da62a1740fb94fab1268802a88e8
James Earl Douglas <james@earldouglas.com>
2021-05-09 15:48:47 -0700

    Change working directory to src/reqs

--- src/reqs/check.sh
+++ src/reqs/check.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+pushd `dirname $0`
+
 function test() {
   echo "scalaVersion := \"$SCALA_VERSION\"" > scalaVersion.sbt
   echo "sbt.version=$SBT_VERSION" > project/build.properties
@@ -9,12 +11,14 @@ function test() {
   cat project/build.properties
   cat scalaVersion.sbt
 
-  curl -s -L $SBT_LAUNCH -o sbt-launch.jar
+  curl -s -L $SBT_LAUNCH -o sbt-launch-$SBT_VERSION.jar
 
-  HOME=`mktemp -d` java -jar sbt-launch.jar clean jetty:test
+  HOME=`mktemp -d` java -jar sbt-launch-$SBT_VERSION.jar clean jetty:test
 }
 
 SCALA_VERSION=2.10.2 SBT_VERSION=0.13.6 SBT_LAUNCH=https://dl.bintray.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.6/sbt-launch.jar test
 SCALA_VERSION=2.10.2 SBT_VERSION=0.13.18 SBT_LAUNCH=https://dl.bintray.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.18/sbt-launch.jar test
 SCALA_VERSION=2.13.4 SBT_VERSION=1.3.0 SBT_LAUNCH=https://repo1.maven.org/maven2/org/scala-sbt/sbt-launch/1.3.0/sbt-launch-1.3.0.jar test
 SCALA_VERSION=2.13.4 SBT_VERSION=1.5.0 SBT_LAUNCH=https://repo1.maven.org/maven2/org/scala-sbt/sbt-launch/1.5.0/sbt-launch-1.5.0.jar test
+
+popd
