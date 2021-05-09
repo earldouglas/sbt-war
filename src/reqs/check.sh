98c00cacda2a035bb552b02b8175aa7b1c251b88
James Earl Douglas <james@earldouglas.com>
2021-05-09 16:15:44 -0700

    Squash the source code for the several reqs tests (#446)
    This de-duplicates the source code for the min/max reqs tests into a
    single project tree that lacks settings for `scalaVersion` and
    `sbt.version`.  The *check.sh* script sets both for each test, which
    makes maintenance less of a hassle.

--- src/reqs/check.sh
+++ src/reqs/check.sh
@@ -2,12 +2,23 @@
 
 set -e
 
-for f in $(dirname $0)/*
-do
-  if [ -d $f ]
-  then
-    pushd $f
-    sbt jetty:test
-    popd
-  fi
-done
+pushd `dirname $0`
+
+function test() {
+  echo "scalaVersion := \"$SCALA_VERSION\"" > scalaVersion.sbt
+  echo "sbt.version=$SBT_VERSION" > project/build.properties
+
+  cat project/build.properties
+  cat scalaVersion.sbt
+
+  curl -s -L $SBT_LAUNCH -o sbt-launch-$SBT_VERSION.jar
+
+  HOME=`mktemp -d` java -jar sbt-launch-$SBT_VERSION.jar clean jetty:test
+}
+
+SCALA_VERSION=2.10.2 SBT_VERSION=0.13.6 SBT_LAUNCH=https://dl.bintray.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.6/sbt-launch.jar test
+SCALA_VERSION=2.10.2 SBT_VERSION=0.13.18 SBT_LAUNCH=https://dl.bintray.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.18/sbt-launch.jar test
+SCALA_VERSION=2.13.4 SBT_VERSION=1.3.0 SBT_LAUNCH=https://repo1.maven.org/maven2/org/scala-sbt/sbt-launch/1.3.0/sbt-launch-1.3.0.jar test
+SCALA_VERSION=2.13.4 SBT_VERSION=1.5.0 SBT_LAUNCH=https://repo1.maven.org/maven2/org/scala-sbt/sbt-launch/1.5.0/sbt-launch-1.5.0.jar test
+
+popd
