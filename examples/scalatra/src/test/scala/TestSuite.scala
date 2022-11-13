136aa0f560df4033d73a8e057490a2a6eee63359
Scala Steward <james@earldouglas.com>
2022-11-13 16:32:44 -0700

    scalatra: 2.7.0 -> 2.7.1

---
+++ examples/scalatra/src/test/scala/TestSuite.scala
@@ -0,0 +1,18 @@
+import org.scalatest._
+
+class TestSuite extends FunSuite with Matchers {
+
+  test("/") {
+    Request("GET", "http://localhost:8080/", Map.empty, None) shouldBe
+      Response(
+        200,
+        Map("Content-Type" -> "text/html;charset=utf-8"),
+        body =
+          """|<html>
+             |  <body>
+             |    <h1>Hello, world!</h1>
+             |  </body>
+             |</html>""".stripMargin
+      )
+  }
+}
