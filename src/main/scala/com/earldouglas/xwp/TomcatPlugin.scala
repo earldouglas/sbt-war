d7bc29144f3f387aca60d3ea14924a3c785a6dc6
Scala Steward <james@earldouglas.com>
2022-11-13 16:24:57 -0700

    Update webapp-runner from 9.0.41.0 to 9.0.68.0

--- src/main/scala/com/earldouglas/xwp/TomcatPlugin.scala
+++ src/main/scala/com/earldouglas/xwp/TomcatPlugin.scala
@@ -17,7 +17,7 @@ object TomcatPlugin extends AutoPlugin {
 
   override val projectConfigurations = Seq(Tomcat)
 
-  val webappRunner = "com.heroku" % "webapp-runner" % "9.0.41.0"
+  val webappRunner = "com.heroku" % "webapp-runner" % "9.0.68.0"
 
   override lazy val projectSettings =
     ContainerPlugin.containerSettings(Tomcat) ++
