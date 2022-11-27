e517eff6ea3d781b94b71619df57446a478826fe
Scala Steward <james@earldouglas.com>
2022-11-26 19:17:01 -0700

    zio: 2.0.0-M1 -> 2.0.4

--- examples/zio/src/main/scala/JdbcIO.scala
+++ examples/zio/src/main/scala/JdbcIO.scala
@@ -7,27 +7,28 @@ trait JdbcIO {
 object JdbcIO {
 
   import java.sql.SQLException
+  import zio.ZEnvironment
   import zio.ZIO
 
   def effect[A](k: Connection => A): ZIO[JdbcIO, Throwable, A] =
-    ZIO.fromFunctionM { env => ZIO.effect { k(env.connection) } }
+    ZIO.environmentWithZIO(env => ZIO.attempt(k(env.get.connection)))
 
   def transact[R <: JdbcIO, A](
       k: ZIO[R, Throwable, A]
   ): ZIO[R, Throwable, A] =
     ZIO
-      .fromFunction({ env: JdbcIO =>
-        env.connection.setAutoCommit(false)
+      .environmentWith({ env: ZEnvironment[JdbcIO] =>
+        env.get.connection.setAutoCommit(false)
         env
       })
       .flatMap { env =>
         k.map({ a =>
-          env.connection.commit()
-          env.connection.close()
+          env.get.connection.commit()
+          env.get.connection.close()
           a
         }).catchSome({ case x: SQLException =>
-          env.connection.rollback()
-          env.connection.close()
+          env.get.connection.rollback()
+          env.get.connection.close()
           ZIO.fail(x)
         })
       }
