21f71aad0ffa1edc811e6d93644f80557d3f07c9
James Earl Douglas <james@earldouglas.com>
2024-09-24 12:17:35 -0700

    Add some comments

--- src/main/scala/com/earldouglas/sbt/war/WarPackagePlugin.scala
+++ src/main/scala/com/earldouglas/sbt/war/WarPackagePlugin.scala
@@ -6,6 +6,14 @@ import sbt.Keys.moduleName
 import sbt.Keys.{`package` => pkg}
 import sbt._
 
+/** Identifies the files that compose the .war file (resources, .class
+  * files in the classes/ directory, and .jar files in the lib/
+  * directory).
+  *
+  * Also configures the .war file as an sbt package artifact.
+  *
+  * This is also used by other user-facing plugins (WebappRunnerPlugin).
+  */
 object WarPackagePlugin extends AutoPlugin {
 
   override def requires = WebappComponentsPlugin
