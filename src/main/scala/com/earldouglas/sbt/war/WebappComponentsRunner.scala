4568bbd0687450b981df53b1c4a24b1e619bcae7
James Earl Douglas <james@earldouglas.com>
2024-09-27 04:58:48 -0700

    WebappComponentsRunner[Plugin]: init (#939)

---
+++ src/main/scala/com/earldouglas/sbt/war/WebappComponentsRunner.scala
@@ -0,0 +1,117 @@
+package com.earldouglas.sbt.war
+
+import org.apache.catalina.Context
+import org.apache.catalina.WebResourceRoot
+import org.apache.catalina.connector.Connector
+import org.apache.catalina.startup.Tomcat
+import org.apache.catalina.webresources.DirResourceSet
+import org.apache.catalina.webresources.FileResourceSet
+import org.apache.catalina.webresources.StandardRoot
+
+import java.io.File
+import java.nio.file.FileAlreadyExistsException
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Path
+
+trait WebappComponentsRunner {
+  def start(): Unit
+  def join(): Unit
+  def stop(): Unit
+}
+
+/** Launches a webapp composed of in-place resources, classes, and
+  * libraries.
+  *
+  * emptyWebappDir and emptyClassesDir need to point to one or two empty
+  * directories. They're not used to serve any content, but they are
+  * required by Tomcat's internals.
+  *
+  * To use a root context path (i.e. /), set contextPath to the empty
+  * string for some reason.
+  */
+object WebappComponentsRunner {
+
+  def mkdir(file: File): File = {
+    if (file.exists()) {
+      if (!file.isDirectory()) {
+        throw new FileAlreadyExistsException(file.getPath())
+      } else {
+        file
+      }
+    } else {
+      val path: Path =
+        FileSystems
+          .getDefault()
+          .getPath("target", "empty")
+      try {
+        Files.createDirectory(path)
+        file
+      } catch {
+        case e: FileAlreadyExistsException =>
+          file
+      }
+    }
+  }
+
+  def apply(
+      hostname: String,
+      port: Int,
+      contextPath: String,
+      emptyWebappDir: File,
+      emptyClassesDir: File,
+      resourceMap: Map[String, File]
+  ): WebappComponentsRunner = {
+
+    val tomcat: Tomcat = new Tomcat()
+    tomcat.setHostname(hostname)
+
+    val connector: Connector = new Connector()
+    connector.setPort(port)
+    tomcat.setConnector(connector)
+
+    val context: Context =
+      tomcat
+        .addWebapp(
+          contextPath,
+          emptyWebappDir.getAbsolutePath()
+        )
+
+    val webResourceRoot: WebResourceRoot =
+      new StandardRoot(context)
+
+    webResourceRoot.addJarResources(
+      new DirResourceSet(
+        webResourceRoot,
+        "/WEB-INF/classes",
+        emptyClassesDir.getAbsolutePath(),
+        "/"
+      )
+    )
+
+    resourceMap
+      .filter({ case (_, file) => file.exists() })
+      .filter({ case (_, file) => file.isFile() })
+      .foreach { case (path, file) =>
+        webResourceRoot.addJarResources(
+          new FileResourceSet(
+            webResourceRoot,
+            s"/${path}",
+            file.getAbsolutePath(),
+            "/"
+          )
+        )
+      }
+
+    context.setResources(webResourceRoot)
+
+    new WebappComponentsRunner {
+      override def start(): Unit = tomcat.start()
+      override def join(): Unit = tomcat.getServer().await()
+      override def stop(): Unit = {
+        tomcat.stop()
+        tomcat.destroy()
+      }
+    }
+  }
+}
---
+++ src/main/scala/com/earldouglas/sbt/war/WebappComponentsRunnerPlugin.scala
@@ -0,0 +1,77 @@
+package com.earldouglas.sbt.war
+
+import sbt.Def.Initialize
+import sbt.Def.settingKey
+import sbt.Def.taskKey
+import sbt.Keys._
+import sbt._
+
+import java.util.concurrent.atomic.AtomicReference
+
+/** Launches the webapp composed of the resources, classes, and
+  * libraries specified by WebappComponentsPlugin.
+  */
+object WebappComponentsRunnerPlugin extends AutoPlugin {
+
+  object autoImport {
+    lazy val webappPort = settingKey[Int]("container port")
+    lazy val webappStart = taskKey[Unit]("start container")
+    lazy val webappJoin = taskKey[Unit]("join container")
+    lazy val webappStop = taskKey[Unit]("stop container")
+  }
+
+  import autoImport._
+
+  override val requires: Plugins = WebappComponentsPlugin
+
+  private lazy val containerInstance =
+    new AtomicReference[Option[WebappComponentsRunner]](None)
+
+  private val startWebapp: Initialize[Task[Unit]] =
+    Def.task {
+      stopContainerInstance()
+
+      val emptyDir: File =
+        WebappComponentsRunner.mkdir(
+          (Compile / target).value / "empty"
+        )
+
+      val runner: WebappComponentsRunner =
+        WebappComponentsRunner(
+          hostname = "localhost", // TODO this could be a settingKey
+          contextPath = "", // TODO this could be a settingKey
+          port = webappPort.value,
+          emptyWebappDir = emptyDir,
+          emptyClassesDir = emptyDir,
+          resourceMap = WebappComponentsPlugin.webappContents.value
+        )
+      runner.start()
+
+      containerInstance.set(Some(runner))
+    }
+
+  private val joinWebapp: Initialize[Task[Unit]] =
+    Def.task(containerInstance.get.foreach(_.join()))
+
+  private def stopContainerInstance(): Unit =
+    containerInstance.getAndSet(None).foreach(_.stop())
+
+  private val stopWebapp: Initialize[Task[Unit]] =
+    Def.task(stopContainerInstance())
+
+  private val onLoadSetting: Initialize[State => State] =
+    Def.setting {
+      (Global / onLoad).value compose { state: State =>
+        state.addExitHook(stopContainerInstance())
+      }
+    }
+
+  override lazy val projectSettings: Seq[Setting[_]] =
+    Seq(
+      webappPort := 8080,
+      webappStart := startWebapp.value,
+      webappJoin := joinWebapp.value,
+      webappStop := stopWebapp.value,
+      Global / onLoad := onLoadSetting.value
+    )
+}
