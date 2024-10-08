0e7c9ed4da77a5e02a1403a4ede1e4b222bb4814
James Earl Douglas <james@earldouglas.com>
2024-10-08 07:27:10 -0700

    Run webapp components in a forked JVM
    This drops the webapp components runner in favor of
    com.earldouglas:webapp-components-runner to launch the webapp components
    in a forked JVM.

--- src/main/scala/com/earldouglas/sbt/war/WebappComponentsRunnerPlugin.scala
+++ src/main/scala/com/earldouglas/sbt/war/WebappComponentsRunnerPlugin.scala
@@ -1,13 +1,18 @@
 package com.earldouglas.sbt.war
 
 import sbt.Def.Initialize
+import sbt.Def.settingKey
 import sbt.Keys._
 import sbt._
 
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.concurrent.atomic.AtomicReference
+import scala.sys.process.{Process => ScalaProcess}
 
-/** Launches the webapp composed of the resources, classes, and
-  * libraries specified by WebappComponentsPlugin.
+/** Launches the webapp file managed by WebappComponentsPlugin. Uses a
+  * forked JVM to run Tomcat via
+  * com.earldouglas:webapp-components-runner.
   */
 object WebappComponentsRunnerPlugin extends AutoPlugin {
 
@@ -17,55 +22,96 @@ object WebappComponentsRunnerPlugin extends AutoPlugin {
     lazy val webappStart = taskKey[Unit]("start webapp container")
     lazy val webappJoin = taskKey[Unit]("join webapp container")
     lazy val webappStop = taskKey[Unit]("stop webapp container")
+    lazy val webappForkOptions =
+      settingKey[ForkOptions]("webapp container fork options")
+    lazy val webappComponentsRunnerVersion =
+      settingKey[String]("webapp-components-runner version")
   }
 
   import autoImport._
+  import WebappRunnerPlugin.autoImport._
 
   override val requires: Plugins =
-    WebappComponentsPlugin
+    WebappComponentsPlugin && WebappRunnerPlugin
+
+  override val projectConfigurations: Seq[Configuration] =
+    Seq(Webapp)
 
   private lazy val containerInstance =
-    new AtomicReference[Option[WebappComponentsRunner]](None)
+    new AtomicReference[Option[ScalaProcess]](None)
 
   override val projectSettings: Seq[Setting[_]] = {
 
-    def stopContainerInstance(): Unit =
-      containerInstance.getAndSet(None).foreach(_.stop())
+    def stopContainerInstance(): Unit = {
+      val oldProcess = containerInstance.getAndSet(None)
+      oldProcess.foreach(_.destroy())
+    }
+
+    val runnerConfigFile: Initialize[Task[File]] =
+      Def.task {
+
+        val emptyDir: File = (Compile / target).value / "empty"
+
+        val resourceMapString =
+          WebappComponentsPlugin.webappContents.value
+            .map { case (k, v) =>
+              s"${k}->${v}"
+            }
+            .mkString(",")
+
+        val configurationFile: File =
+          (Compile / target).value / "webapp-components.properties"
+
+        Files
+          .writeString(
+            Paths.get(configurationFile.getPath()),
+            s"""|hostname=localhost
+              |port=${webappPort.value}
+              |contextPath=
+              |emptyWebappDir=${emptyDir}
+              |emptyClassesDir=${emptyDir}
+              |resourceMap=${resourceMapString}
+              |""".stripMargin
+          )
+          .toFile()
+      }
 
     val startWebapp: Initialize[Task[Unit]] =
       Def.task {
         stopContainerInstance()
 
-        val emptyDir: File =
-          WebappComponentsRunner.mkdir(
-            (Compile / target).value / "empty"
-          )
+        val runnerJars: Seq[File] =
+          Classpaths
+            .managedJars(Webapp, classpathTypes.value, update.value)
+            .map(_.data)
+            .toList
 
-        val runner: WebappComponentsRunner =
-          WebappComponentsRunner(
-            hostname = "localhost", // TODO this could be a settingKey
-            port = webappPort.value,
-            contextPath = "", // TODO this could be a settingKey
-            emptyWebappDir = emptyDir,
-            emptyClassesDir = emptyDir,
-            resourceMap = WebappComponentsPlugin.webappContents.value
+        streams.value.log.info("[sbt-war] Starting server")
+        val process: ScalaProcess =
+          Fork.java.fork(
+            webappForkOptions.value,
+            Seq(
+              "-cp",
+              Path.makeString(runnerJars),
+              "com.earldouglas.WebappComponentsRunner",
+              runnerConfigFile.value.getPath()
+            )
           )
-        runner.start()
-
-        containerInstance.set(Some(runner))
+        containerInstance.set(Some(process))
       }
 
     val joinWebapp: Initialize[Task[Unit]] =
-      Def.task(containerInstance.get.foreach(_.join()))
+      Def.task(containerInstance.get.map(_.exitValue))
 
     val stopWebapp: Initialize[Task[Unit]] =
       Def.task(stopContainerInstance())
 
     val onLoadSetting: Initialize[State => State] =
       Def.setting {
-        (Global / onLoad).value compose { state: State =>
-          state.addExitHook(stopContainerInstance())
-        }
+        (Global / onLoad).value
+          .compose { state: State =>
+            state.addExitHook(stopContainerInstance())
+          }
       }
 
     Seq(
@@ -73,7 +119,14 @@ object WebappComponentsRunnerPlugin extends AutoPlugin {
       webappStart := startWebapp.value,
       webappJoin := joinWebapp.value,
       webappStop := stopWebapp.value,
-      Global / onLoad := onLoadSetting.value
+      webappForkOptions := ForkOptions(),
+      Global / onLoad := onLoadSetting.value,
+      webappComponentsRunnerVersion := BuildInfo.webappComponentsRunnerVersion,
+      libraryDependencies ++=
+        Seq(
+          "com.earldouglas" % "webapp-components-runner" % webappComponentsRunnerVersion.value % Webapp,
+          "com.heroku" % "webapp-runner" % webappRunnerVersion.value % Webapp
+        )
     )
   }
 }
